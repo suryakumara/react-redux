dd5e4056fb6ddfefaa5235e8fb3ca8a9e8973d76
Surya Kumara <suryakumara33@gmail.com>
2021-07-10 09:56:13 +0800

    add todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import React, { Component } from "react";
+import AddTodo from "./AddTodo";
+import Todos from "./Todos";
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        content: "buy some milk",
+      },
+      {
+        id: 2,
+        content: "play mario kart",
+      },
+    ],
+  };
+
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+
+    this.setState({
+      todos: todos,
+    });
+  };
+
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos: todos,
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Tes</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
